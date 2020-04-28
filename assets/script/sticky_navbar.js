2493deaed2bb8358b8bdc367b7606b9f1a8d8005
ABTCoder <amalbenson98@gmail.com>
2020-04-28 11:43:43 +0200

    Sticky bug fix

--- assets/script/sticky_navbar.js
+++ assets/script/sticky_navbar.js
@@ -1,17 +1,21 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onload = function() {
+	// When the user scrolls the page, execute myFunction
+	window.onscroll = function() {myFunction()};
 
-// Get the navbar
-var navbar = document.getElementById("topnav");
+	// Get the navbar
+	var navbar = document.getElementById("topnav");
+	var main_content = document.getElementById("main");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+	// Get the offset position of the navbar
+	var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-	if (window.pageYOffset >= sticky) {
-		navbar.classList.add("sticky");
-	} else {
-		navbar.classList.remove("sticky");
+	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+	function myFunction() {
+		if (window.pageYOffset >= sticky) {
+			main_content.style.padding="54px 0 0 0";
+			navbar.classList.add("sticky");
+		} else {
+			navbar.classList.remove("sticky");
+		}
 	}
 }
