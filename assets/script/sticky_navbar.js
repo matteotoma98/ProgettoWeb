3e03d394d1564ea73ffcbaebf060151420213898
GiovanniGregorini <giovigre@gmail.com>
2020-04-29 10:07:35 +0200

    modifica sticky navbar

--- assets/script/sticky_navbar.js
+++ assets/script/sticky_navbar.js
@@ -1,6 +1,8 @@
 window.onload = function() {
 	// When the user scrolls the page, execute myFunction
-	window.onscroll = function() {myFunction()};
+	window.onscroll = function() {myFunction();};
+        // When the window is resized, execute changeHeight
+        window.onresize = function() {changeHeight();};
 
 	// Get the navbar
 	var navbar = document.getElementById("topnav");
@@ -8,14 +10,21 @@ window.onload = function() {
 
 	// Get the offset position of the navbar
 	var sticky = navbar.offsetTop;
+        // Get the height of the navbar
+        var height = navbar.offsetHeight;
 
 	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 	function myFunction() {
 		if (window.pageYOffset >= sticky) {
-			if (main_content!=null) main_content.style.padding="54px 0 0 0";
+			if (main_content!==null) main_content.style.padding=height+"px 0 0 0";
 			navbar.classList.add("sticky");
 		} else {
 			navbar.classList.remove("sticky");
 		}
 	}
-}
+        // Update the height of the navbar and the padding of the main box
+        function changeHeight(){
+            height = navbar.offsetHeight;
+            myFunction();
+        }
+};
